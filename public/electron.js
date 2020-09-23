6ffa2af016b82e4543499dff50123f475d58d181
Anil Kumar <Anil3.Kumar@igglobal.com>
2020-09-23 10:47:08 +0530

    resolved issue for notification

--- public/electron.js
+++ public/electron.js
@@ -1,27 +1,43 @@
 const electron = require("electron");
 const app = electron.app;
 const Menu= electron.Menu
+const Notification=electron.Notification
 const BrowserWindow = electron.BrowserWindow;
+const ipcMain = electron.ipcMain;
+
 const path = require("path");
 const isDev = require("electron-is-dev");
+
 let mainWindow;
+// window.ipcRenderer = require('electron').ipcRenderer;
+
 function createWindow() {
     mainWindow = new BrowserWindow({ 
         width: 1200, 
         height: 800,
-        icon: ""
+        icon: "",
+        webPreferences: { webSecurity: false, nodeIntegration: true } 
     });
      
 console.warn("some ",`file://${path.join(__dirname, "../build/index.html")}`)
 
     mainWindow.loadURL(
         isDev
-        ? `file://${path.join(__dirname, "../build/index.html")}`
+        ? `http://localhost:3000/`
         :`file://${path.join(__dirname, "../build/index.html")}`
     );
     mainWindow.on("closed", () => (mainWindow = null));
 }
  
+// setTimeout(()=>{
+//     notification = new Notification({title: "Message from: ",body: "messagebody"})
+// notification.show()
+// notification.on('click', (event, arg)=>{
+//     mainWindow.moveTop()
+//     mainWindow.focus()
+// })
+// },2000)
+
 // const menu = Menu.buildFromTemplate([{
 //     label: 'File',
 //     submenu: [
@@ -29,6 +45,19 @@ console.warn("some ",`file://${path.join(__dirname, "../build/index.html")}`)
 //     ]
 //   },])
 // Menu.setApplicationMenu(menu)
+// 
+ipcMain.on('notiGet', (event, arg) => {
+    console.warn("done in main file",arg) // prints "ping"
+    setTimeout(()=>{
+    notification = new Notification({title: "Message from: ",body: "messagebody"})
+notification.show()
+notification.on('click', (event, arg)=>{
+    mainWindow.moveTop()
+    mainWindow.focus()
+})
+},1000)
+  })
+// 
 app.on("ready", createWindow);
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") {
--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 import HomeContainer from './containers/HomeContainer'
 import HeaderContainer from './containers/HeaderContainer'
+const  {ipcRenderer}  = window.require('electron');
+// const { ipcRenderer, remote } = require('electron');
+
+// const { ipcRenderer } = require('electron');
+// console.warn("app data",Notificaion)
 function App() {
+//   setTimeout(()=>{
+//    var  notification = new Notification("Message from: ")
+//   // const notification = new Notification(title, options);
+
+//   notification.onclick = function() {
+//     console.warn('Notification clicked')
+//     window.focus(); //just in case, older browsers
+//   }
+// },3000)
+const dataFunction=()=>{
+  console.warn("done");
+  ipcRenderer.send('notiGet','a string', 10);
+}
   return (
     <div className="App">
       <HeaderContainer />
-     <button onClick={()=>alert("some data")} >Click</button>
+     <button onClick={()=>dataFunction()} >Click1</button>
       <HomeContainer />
     </div>
   );
-}
+};
 
 export default App;
